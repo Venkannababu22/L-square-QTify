99a7f3a2efe3eb3631f3d541deb005a7b93c83a4
VENKANNABABU <yellamellivenkannababu@gmail.com>
2023-12-20 00:38:41 +0530

    First commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import React from "react"
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero"
 
 function App() {
+  const [searchData, setSearchData] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar searchData={searchData}></Navbar>
+      <Hero />
+    </>
   );
 }
 
---
+++ qtify/src/Components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import LogoImage from "../../Assests/Qtify-Logo.png"
+
+function Logo(){
+    return(
+        <img src={LogoImage} alt="Logo" width={67}/>
+    )
+}
+
+export default Logo;
